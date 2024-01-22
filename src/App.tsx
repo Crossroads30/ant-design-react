32662027c0ca854ea9dfc415449707dac45bd504
crossroads30 <crossroads30@mail.ru>
2024-01-22 16:40:53 +0300

    feat: add ant design

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { DatePicker } from 'antd'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
- <h1>Test</h1>
-    </>
-  )
+const App = () => {
+	return <DatePicker />
 }
 
 export default App
